7b3e2c9a046639d336d5d6c2b9bc4d9f4c057913
apple11361 <apple113611361@gmail.com>
2017-06-10 14:51:19 +0800

    basic interface and pot-btn

--- Plant_Word/Plant_Word/Form1.Designer.cs
+++ Plant_Word/Plant_Word/Form1.Designer.cs
@@ -29,21 +29,104 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Store = new System.Windows.Forms.Button();
+            this.Quest = new System.Windows.Forms.Button();
+            this.Item = new System.Windows.Forms.Button();
+            this.Achievement = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.farm_panel = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Store
+            // 
+            this.Store.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Store.Location = new System.Drawing.Point(45, 3);
+            this.Store.Margin = new System.Windows.Forms.Padding(45, 3, 45, 3);
+            this.Store.Name = "Store";
+            this.Store.Size = new System.Drawing.Size(120, 35);
+            this.Store.TabIndex = 1;
+            this.Store.Text = "商店";
+            this.Store.UseVisualStyleBackColor = true;
+            this.Store.Click += new System.EventHandler(this.Store_Click);
+            // 
+            // Quest
+            // 
+            this.Quest.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Quest.Location = new System.Drawing.Point(255, 3);
+            this.Quest.Margin = new System.Windows.Forms.Padding(45, 3, 45, 3);
+            this.Quest.Name = "Quest";
+            this.Quest.Size = new System.Drawing.Size(120, 35);
+            this.Quest.TabIndex = 2;
+            this.Quest.Text = "任務";
+            this.Quest.UseVisualStyleBackColor = true;
+            // 
+            // Item
+            // 
+            this.Item.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Item.Location = new System.Drawing.Point(465, 3);
+            this.Item.Margin = new System.Windows.Forms.Padding(45, 3, 45, 3);
+            this.Item.Name = "Item";
+            this.Item.Size = new System.Drawing.Size(120, 35);
+            this.Item.TabIndex = 3;
+            this.Item.Text = "道具";
+            this.Item.UseVisualStyleBackColor = true;
+            // 
+            // Achievement
+            // 
+            this.Achievement.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Achievement.Location = new System.Drawing.Point(675, 3);
+            this.Achievement.Margin = new System.Windows.Forms.Padding(45, 3, 45, 3);
+            this.Achievement.Name = "Achievement";
+            this.Achievement.Size = new System.Drawing.Size(120, 35);
+            this.Achievement.TabIndex = 4;
+            this.Achievement.Text = "成就";
+            this.Achievement.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.Transparent;
+            this.flowLayoutPanel2.Controls.Add(this.Store);
+            this.flowLayoutPanel2.Controls.Add(this.Quest);
+            this.flowLayoutPanel2.Controls.Add(this.Item);
+            this.flowLayoutPanel2.Controls.Add(this.Achievement);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(75, 20);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(840, 44);
+            this.flowLayoutPanel2.TabIndex = 5;
+            // 
+            // farm_panel
+            // 
+            this.farm_panel.BackColor = System.Drawing.Color.Transparent;
+            this.farm_panel.Location = new System.Drawing.Point(100, 160);
+            this.farm_panel.Name = "farm_panel";
+            this.farm_panel.Size = new System.Drawing.Size(800, 550);
+            this.farm_panel.TabIndex = 6;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Plant_Word.Properties.Resources.background;
             this.ClientSize = new System.Drawing.Size(1008, 730);
+            this.Controls.Add(this.farm_panel);
+            this.Controls.Add(this.flowLayoutPanel2);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "蔡英文";
+            this.Text = "菜英文";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.flowLayoutPanel2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private System.Windows.Forms.Button Store;
+        private System.Windows.Forms.Button Quest;
+        private System.Windows.Forms.Button Item;
+        private System.Windows.Forms.Button Achievement;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Panel farm_panel;
     }
 }
 
--- Plant_Word/Plant_Word/Form1.cs
+++ Plant_Word/Plant_Word/Form1.cs
@@ -12,9 +12,57 @@ namespace Plant_Word
 {
     public partial class Form1 : Form
     {
+        private
+        Item item_form = new Item();
+        Store store_form = new Store();
+        int[] pot = new int[12];
+        Button[] pot_btn = new Button[12]; 
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        public void init()
+        {
+            int i;
+
+            /************load pot************/
+            for(i=0;i<12;i++)
+            {
+                pot_btn[i] = new Button();
+                pot_btn[i].Height = 70;
+                pot_btn[i].Width = 70;
+                pot_btn[i].Location = new Point(15 + i / 2 * 140, 340 + i % 2 * 110);
+                pot_btn[i].BackColor = Color.Transparent;
+                pot_btn[i].FlatStyle = FlatStyle.Popup;
+                pot_btn[i].Name = i.ToString();
+                pot_btn[i].Text = pot_btn[i].Name;
+                pot_btn[i].Click += new EventHandler(pot_btn_Click);
+                this.farm_panel.Controls.Add(pot_btn[i]);
+            }
+
+            /**********set form owner*********/
+            item_form.Owner = this;
+            store_form.Owner = this;
+        }
+
+        private void Store_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pot_btn_Click(object sender, EventArgs e)
+        {
+            if(pot[int.Parse(((Button)sender).Name)] == 0)
+            {
+                item_form.ShowDialog();
+            }
         }
     }
 }
